81e27226a04d597b40cacab09d4aec4583567827
jvansant13 <jermey.vansant@davinciacademy.org>
2025-09-17 09:59:35 -0600

    modularized my current plugins and added lualine at the bottom

--- init.lua
+++ init.lua
@@ -1,9 +1,3 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.g.mapleader = " "
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -20,39 +14,5 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-  {
-    'sainnhe/everforest',
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.g.everforest_enable_italic = true
-      vim.cmd.colorscheme('everforest')
-    end
-  },
-  {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    branch = 'master',
-    lazy = false,
-    build = ":TSUpdate"
-  }
-}
-local opts = {}
 
-
-require("lazy").setup(plugins, opts)
-local builtin = require("telescope.builtin")
-local config = require("nvim-treesitter.configs")
-config.setup({
-  ensure_installed = {"lua", "python", "rust"},
-  highlighting = {enable = true},
-  indent = {enable = true},
-})
-
-vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-
+require("lazy").setup("plugins")
