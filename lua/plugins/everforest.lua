cc27ef8365f4e27a0d6d7338c2d8e398c99947ce
jvansant13 <jermey.vansant@davinciacademy.org>
2025-09-19 20:33:40 -0600

    Added more plugins

--- lua/plugins/everforest.lua
+++ lua/plugins/everforest.lua
@@ -1,8 +1,11 @@
 return {
-    'sainnhe/everforest',
+   "neanias/everforest-nvim",
     lazy = false,
     priority = 1000,
     config = function()
+       require("everforest").setup({
+        transparent_background_level = 1
+      })
       vim.g.everforest_enable_italic = true
       vim.cmd.colorscheme('everforest')
     end
